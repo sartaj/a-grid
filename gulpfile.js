e024a39b3c5205896837c248d3f85b2e16800993
Sartaj <sartaj@sartaj.me>
2015-04-13 23:17:07 -0500

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,51 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
-var bump = require('gulp-bump');
+// Packages
 
-gulp.task('build', ['scripts'])
+  var gulp = require('gulp');
 
-gulp.task('release', ['scripts', 'bump']);
+  var browserify = require('gulp-browserify');
 
-// Basic usage 
-gulp.task('scripts', function() {
-    // Single entry point to browserify 
-    gulp.src('src/eg-grid.js')
-        .pipe(browserify({
-          insertGlobals : false,
-          // debug : true
-        }))
-        .pipe(gulp.dest('./dist'));
-});
+  var git = require('gulp-git'),
+      bump = require('gulp-bump'),
+      filter = require('gulp-filter'),
+      tag_version = require('gulp-tag-version');
 
+// Tasks
 
-// https://www.npmjs.com/package/gulp-bump
-gulp.task('bump', function(){
-  gulp.src('./*.json')
-  .pipe(bump({type:'minor'}))
-  .pipe(gulp.dest('./'));
-});
+  gulp.task('build', ['scripts']);
+
+  gulp.task('scripts', function() { return scripts()});
+
+  var releaseTasks = ['build'];
+  gulp.task('patch', releaseTasks , function() { return release('patch'); })
+  gulp.task('minor', releaseTasks , function() { return release('minor'); })
+  gulp.task('major', releaseTasks , function() { return release('major'); })
+
+// Scripts
+
+  function scripts() {
+      // Single entry point to browserify 
+      gulp.src('src/eg-grid.js')
+          .pipe(browserify({
+            insertGlobals : false,
+            // debug : true
+          }))
+          .pipe(gulp.dest('./dist'));
+  }
+
+// Release
+
+  function release(importance) {
+    // get all the files to bump version in 
+    return gulp.src(['./*.json'])
+      // bump the version number in those files 
+      .pipe(bump({type: importance}))
+      // save it back to filesystem 
+      .pipe(gulp.dest('./'))
+      // commit the changed version number 
+      .pipe(git.commit('update version'))
+
+      // read only one file to get the version number 
+      .pipe(filter('package.json'))
+      // **tag it in the repository** 
+      .pipe(tag_version());
+  }
